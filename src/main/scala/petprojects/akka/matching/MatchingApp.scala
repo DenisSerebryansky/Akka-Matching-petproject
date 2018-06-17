9fec9dae5a3fc5a4b66f41beb07940684a975561
Denis Serebryansky <denis.serebryansky.eu@gmail.com>
2018-06-17 14:24:21 +0300

    MatchingApp description

--- src/main/scala/petprojects/akka/matching/MatchingApp.scala
+++ src/main/scala/petprojects/akka/matching/MatchingApp.scala
@@ -11,11 +11,19 @@ import scala.concurrent.duration._
 
 object MatchingApp extends App {
 
+  // create main actors
+
   val system = ActorSystem("Matching")
   val exchange = system.actorOf(Exchange.props("/clients.txt"), "Exchange")
   val orderGenerator = system.actorOf(OrdersGenerator.props, "OrderGenerator")
 
+  // timeout for ask pattern
+
   implicit val timeout: Timeout = Timeout(5 seconds)
+
+  // 1. ask orderGenerator to start generate orders and send them to the exchange actor;
+  // 2. receive GeneratingFinished message from the orderGenerator and request client balances;
+  // 3. save client balances and terminate ActorSystem.
 
   (orderGenerator ? OrdersGenerator.StartGenerateOrders("/orders.txt", exchange))
     .flatMap { case OrdersGenerator.GeneratingFinished ⇒ exchange ? Exchange.QueryClientBalances() }
